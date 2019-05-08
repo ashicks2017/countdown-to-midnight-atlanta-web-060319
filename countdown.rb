4da1efd33226a8f8a5e25a8b4ff34a157fe87294
aeisha simmons <ashicks2017@outlook.com>
2019-05-08 17:37:16 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,20 @@
 #write your code here
-
-def countdown
+number = 10
+def countdown(number)
+ while number >= 1
+ puts "#{number} SECONDS!"
+ number -= 1
+ 
+ end
+   "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep(number)
+  while number > 0
+  number -= 1
+  sleep (1)
+end
+ "HAPPY NEW YEAR!"
+end
+countdown(number)
+countdown_with_sleep(number)
